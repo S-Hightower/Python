afee97cbc28d011980c9c1a1d82cf076d5711059
Shawna Hightower <sdhightower6@gmail.com>
2021-10-27 19:23:43 -0600

    init commit

--- flask/fundamentals/Hello Flask/server.py
+++ flask/fundamentals/Hello Flask/server.py
@@ -1,8 +1,23 @@
-from flask import Flask  # Import Flask to allow us to create our app
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+from flask import Flask 
+ # Import Flask to allow us to create our app
+app = Flask(__name__)
+    # Create a new instance of the Flask class called "app"
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+    return 'Hello World!'
+      # Return the string 'Hello World!' as a response
+
+@app.route('/dojo')
+def dojo():
+    return 'Dojo!'
+
+@app.route('/hello/<name>')
+def hello(name):
+    return f"Hello {name}"
+
+@app.route('/<word>/<int:num>')
+def repeat(word, num):
+    return f"{word * num}"
+
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
-

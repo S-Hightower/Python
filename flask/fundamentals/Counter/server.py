7e599a0ee9016305a89323c7f2e211794699584c
Shawna Hightower <sdhightower6@gmail.com>
2021-10-30 15:00:06 -0600

    updates and corrections

--- flask/fundamentals/Counter/server.py
+++ flask/fundamentals/Counter/server.py
@@ -1,18 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session
 
 app = Flask(__name__)
+app.secret_key='keep is secret, keep it safe'
 
 @app.route('/')
 def server_init():
-    return render_template('index.html')
+    if 'count' not in session:
+        session['count']=0
+    session['count'] += 1
+    return render_template('index.html', count=session['count'])
 
 @app.route('/destroy_session')
 def destroy_session():
     return 
 
-# @app.route('/count')
-# def server_count():
-#     return render_template('index.html')
+@app.route('/reset')
+def reset():
+    session['count']=0
+    return redirect('/')
 
 if __name__=="__main__":
     app.run(debug=True)

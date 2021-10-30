6564c5345eaac5afbb397f58301c3d424d6ccc31
Shawna Hightower <sdhightower6@gmail.com>
2021-10-30 15:31:51 -0600

    project completion

--- flask/fundamentals/Counter/server.py
+++ flask/fundamentals/Counter/server.py
@@ -6,18 +6,22 @@ app.secret_key='keep is secret, keep it safe'
 @app.route('/')
 def server_init():
     if 'count' not in session:
-        session['count']=0
-    session['count'] += 1
+        session['count']=1
+    else:
+        session['count'] += 1
+    print(session['count'])
     return render_template('index.html', count=session['count'])
 
 @app.route('/destroy_session')
 def destroy_session():
-    return 
+    session.clear()
+    return redirect ('/')
 
-@app.route('/reset')
-def reset():
-    session['count']=0
-    return redirect('/')
+@app.route('/add')
+def add():
+    session['count'] += 1
+    print(session['count'])
+    return redirect ('/')
 
 if __name__=="__main__":
     app.run(debug=True)

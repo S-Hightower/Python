118bdac54cc057afb6a239c0d95e22d8c1c923b7
Shawna Hightower <sdhightower6@gmail.com>
2021-11-03 14:50:44 -0600

    adding files and updating connections

--- flask_mysql/crud/users_assignment/server.py
+++ flask_mysql/crud/users_assignment/server.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, redirect, session
 
+from user import User
 app = Flask(__name__)
 app.secret_key='keep is secret, keep it safe'
 
 @app.route('/')
-def server_init():
-    return "Connected"
+def index():
+    users = user.get_all()
+    print(users)
+    return render_template("read(all).html", all_users = users)
+
+@app.route('/new')
+def 
 
 if __name__=="__main__":
     app.run(debug=True)

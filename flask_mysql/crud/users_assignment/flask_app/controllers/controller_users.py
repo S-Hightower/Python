b3d0173576aed0dccd44f3ac10c6d24e8344f890
Shawna Hightower <sdhightower6@gmail.com>
2021-11-05 14:04:37 -0600

    more corrections

--- flask_mysql/crud/users_assignment/flask_app/controllers/controller_users.py
+++ flask_mysql/crud/users_assignment/flask_app/controllers/controller_users.py
@@ -1,7 +1,6 @@
 from flask_app import app
 from flask import render_template, redirect, request, session
 from flask_app.models.users import User
-from flask.globals import request
 
 @app.route('/')
 def index():
@@ -9,15 +8,17 @@ def index():
     print(users)
     return render_template("read(all).html", all_users = users)
 
-@app.route('/new', methods=['POST'])
+@app.route('/new/create', methods=['POST'])
 def add_user():
-    id= users.User.create(request.form)
-    print(id)
+    id = User.add_user(request.form)
     return redirect('/')
 
-@app.route('/new/<int:id>')
-def show_user(id):
-    context={
-        'User': users.User.get_one({'id': id})
-    }
-    return render_template('create.html', **context)
+@app.route('/new')
+def new_user():
+    return render_template('create.html')
+
+# @app.route('/show/<int:id>')
+# def show_user(id):
+#     context={
+#         'User': users.User.get_one({'id': id})
+#     }

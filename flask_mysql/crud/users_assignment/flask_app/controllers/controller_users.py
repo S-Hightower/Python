c7ae5a85ffad8a2f26edd8ab1f5e29ef1664f053
Shawna Hightower <sdhightower6@gmail.com>
2021-11-04 20:14:01 -0600

    corrections

--- flask_mysql/crud/users_assignment/flask_app/controllers/controller_users.py
+++ flask_mysql/crud/users_assignment/flask_app/controllers/controller_users.py
@@ -1,3 +1,16 @@
 from flask_app import app
 from flask import render_template, redirect, request, session
-from models.users import User
+from flask_app.models.users import User
+
+@app.route('/')
+def index():
+    users = User.get_all()
+    print(users)
+    return render_template("flask_app.templates.read(all).html", all_users = users)
+
+@app.route('/new', methods=['POST'])
+def add_user(data):
+    id= users.User.create(request.form)
+    return render_template("flask_app.templates.create.html")
+
+    return redirect('/')
--- flask_mysql/crud/users_assignment/server.py
+++ flask_mysql/crud/users_assignment/server.py
@@ -1,25 +1,7 @@
 from flask.globals import request
 
 from flask_app import app
-from flask_app.controllers import controller
-
-@app.route('/')
-def index():
-    users = User.get_all()
-    print(users)
-    return render_template("flask_app.templates.read(all).html", all_users = users)
-
-@app.route('/new')
-def add_user(data):
-    data = {
-        'first_name': request.form['first_name'],
-        'last_name': request.form['last_name'],
-        'email': request.form['email']
-    }
-    User.save(data)
-    return render_template("create.html")
-
-    return redirect('/')
+from flask_app.controllers import controller_users
     
 if __name__=="__main__":
     app.run(debug=True)

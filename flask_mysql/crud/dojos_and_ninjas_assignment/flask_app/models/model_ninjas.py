d4bf03da66b1c48a2a2a8f11fe4c35af02134643
Shawna Hightower <sdhightower6@gmail.com>
2021-11-07 12:08:27 -0700

    corrections

--- flask_mysql/crud/dojos_and_ninjas_assignment/flask_app/models/model_ninjas.py
+++ flask_mysql/crud/dojos_and_ninjas_assignment/flask_app/models/model_ninjas.py
@@ -1,28 +1,28 @@
 # import the function that will return an instance of a connection
 from flask_app.config.mysqlconnection import connectToMySQL
 
-DATABASE='users_schema'
-# model the class after the friend table from our database
-class User:
+DATABASE='dojos_and_ninjas'
+
+class Ninja:
     def __init__( self , data ):
         self.id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
-        self.email = data['email']
+        self.age = data['age']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
     # Now we use class methods to query our database
     @classmethod
-    def get_all(cls):
-        query = "SELECT * FROM users;"
+    def get_all_ninjas(cls):
+        query = "SELECT * FROM ninjas;"
         # make sure to call the connectToMySQL function with the schema you are targeting.
         results = connectToMySQL(DATABASE).query_db(query)
         # Create an empty list to append our instances of friends
-        users = []
+        ninjas = []
         # Iterate over the db results and create instances of friends with cls.
-        for user in results:
-            users.append( cls(user) )
-        return users
+        for ninja in results:
+            ninjas.append( cls(ninja) )
+        return ninjas
 
     @classmethod
     def add_user(cls, data:dict):
@@ -30,19 +30,26 @@ class User:
         return connectToMySQL(DATABASE).query_db(query, data)
 
     @classmethod
-    def get_one(cls, data:dict):
-        query = "SELECT * FROM users WHERE id = %(id)s"
+    def get_one_dojo_with_ninjas(cls, data:dict):
+        query = "SELECT * FROM dojos LEFT JOIN ninjas on ninjas.dojo_id = dojos.id WHERE dojos.id = %(dojo.id)s"
         results = connectToMySQL(DATABASE).query_db(query, data)
-        if not results:
-            return False
-        return cls(results[0])
+        dojo = cls(results[0])
+        for row_from_db in results:
+            ninja_data = {
+                "id": row_from_db["ninjas.id"],
+                "first_name": row_from_db["ninjas.first_name"],
+                "last_name": row_from_db["ninjas.last_name"],
+                "age": row_from_db["ninjas.age"]
+            }
+        dojo.ninjas.append(Ninja{ninja_data})
+        return dojo
 
-    @classmethod
-    def update_one(cls, data):
-        query="UPDATE users SET first_name=%(first_name)s, last_name=%(last_name)s, email=%(email)s WHERE id = %(id)s"
-        return connectToMySQL(DATABASE).query_db(query, data)
+    # @classmethod
+    # def update_one(cls, data):
+    #     query="UPDATE users SET first_name=%(first_name)s, last_name=%(last_name)s, email=%(email)s WHERE id = %(id)s"
+    #     return connectToMySQL(DATABASE).query_db(query, data)
 
-    @classmethod
-    def delete_one(cls, data):
-        query="DELETE FROM users WHERE id=%(id)s"
-        return connectToMySQL(DATABASE).query_db(query, data)
+    # @classmethod
+    # def delete_one(cls, data):
+    #     query="DELETE FROM users WHERE id=%(id)s"
+    #     return connectToMySQL(DATABASE).query_db(query, data)
